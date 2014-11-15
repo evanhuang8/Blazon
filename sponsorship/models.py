7e1f7a75f1514ef542ee087cfbd3d5b8761b0f31
Evan Huang <evanhuang8@gmail.com>
2014-11-15 17:09:55 +0000

    layout

--- sponsorship/models.py
+++ sponsorship/models.py
@@ -1,11 +1,25 @@
+from django.conf import settings
 from django.db import models
 
 class Campaign(models.Model):
 
   name = models.CharField(max_length = 100)
   description = models.TextField()
+  created_by = models.ForeignKey(settings.AUTH_MODEL)
+  created_at = models.DateTimeField(auto_now_add = True)
+  updated_at = models.DateTimeField(auto_now = True)
 
 class Tier(models.Model):
 
+  campaign = models.ForeignKey('Campaign')
   name = models.CharField(max_length = 100)
   description = models.TextField()
+  price = models.IntegerField()
+  created_at = models.DateTimeField(auto_now_add = True)
+
+class Inkind(models.Model):
+
+  campaign = models.ForeignKey('Campaign')
+  name = models.CharField(max_length = 100)
+  description = models.TextField()
+  created_at = models.DateTimeField(auto_now_add = True)
