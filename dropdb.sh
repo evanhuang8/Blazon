090e175f3d1223f770659cc2de2250df804a3217
Evan Huang <evanhuang8@gmail.com>
2014-11-15 18:05:16 +0000

    Custom user models

--- dropdb.sh
+++ dropdb.sh
@@ -4,5 +4,5 @@ mysql -u root -pL33che$ -e 'create database Blazon;'
 rm -f sponsorship/migrations/0*
 python manage.py makemigrations
 python manage.py migrate
-echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@blazon.com', 'blazon')" | python manage.py shell
+echo "from sponsorship.models import User; User.objects.create_superuser('admin@blazon.us', 'Blazon', 'Admin', 'blazon')" | python manage.py shell
 echo "\n"
