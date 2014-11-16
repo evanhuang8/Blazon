74eb75ddf36393c2dfba84b38bda9bec456a7640
Andy Garvin <garvin.andy@gmail.com>
2014-11-16 02:08:53 +0000

    Create Campaign Dropdown

--- sponsorship/static/js/blazon.js
+++ sponsorship/static/js/blazon.js
@@ -1,6 +1,13 @@
 (function() {
   this.Blazon = {
-    index: {}
+    index: {},
+    init: function() {
+      $('a.create-campaign-start-btn').click(function() {
+        $('div#create-campaign').slideToggle();
+      });
+    }
   };
+
+  this.Blazon.init();
 
 }).call(this);
--- views/js/blazon.coffee
+++ views/js/blazon.coffee
@@ -1,3 +1,11 @@
 @Blazon = 
 
   index: {}
+
+  init: () ->
+    $('a.create-campaign-start-btn').click () ->
+      $('div#create-campaign').slideToggle()
+      return
+    return
+
+@Blazon.init()
