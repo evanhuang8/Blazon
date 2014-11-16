03d61d2288a3f5d6b3011ddf2d9280aa513e09f9
Evan Huang <evanhuang8@gmail.com>
2014-11-16 01:47:05 +0000

    Url

--- sponsorship/urls.py
+++ sponsorship/urls.py
@@ -5,6 +5,7 @@ urlpatterns = patterns('',
   # Front end
   url(r'^$', index.index, name = 'index'),
   url(r'^login/$', index.login, name = 'login'),
+  url(r'^register/$', index.register, name = 'register'),
   url(r'^dashboard/$', index.dashboard, name = 'dashboard'),
   url(r'^email/$', index.view_email, name = 'view_email'),
   # Back end
